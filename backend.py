b730ad44929aabc51a61777ad32c9d38c6bc1769
alvv <alvv@demant.com>
2023-03-29 15:41:53 +0200

    Working prototype with the transfer to the server

--- backend.py
+++ backend.py
@@ -1,21 +1,36 @@
 import ftplib
 from glob import glob
 import os
+os.path.sep = "/"
 
 class galaxiamover:
     def __init__(self):
         self.hostname = "10.32.64.74"
         self.username = "aoi"
         self.password = "Inspection@2023"
+        self.work_directory = '/'.join( ('AOI_Production', 'AOI_Output') )
         
     def open_connection(self):
-        ftp_server = ftplib.FTP(self.hostname, self.username, self.password)
+        self.ftp_server = ftplib.FTP(self.hostname, self.username, self.password)
+        self.ftp_server.cwd(self.work_directory)
+        
+    def close_connection(self):
+        self.ftp_server.quit()
+        
+    def send_file(self, file_path):
+        filename = os.path.basename(file_path)
+        print(filename)
+        print(self.ftp_server.pwd())
+        with open(file_path, 'rb') as file:
+            self.ftp_server.storbinary('STOR {}'.format( filename ), fp=file)
         
     def get_files_to_transfer(self, path_to_the_file):
         glob_input = os.path.join(path_to_the_file,"*.xml")
         filelist = glob(glob_input)
         return(filelist)
         
-
 gl = galaxiamover()
-print(gl.get_files_to_transfer('Data'))
+file = gl.get_files_to_transfer('Data')
+gl.open_connection()
+gl.send_file(file[0])
+gl.close_connection()
