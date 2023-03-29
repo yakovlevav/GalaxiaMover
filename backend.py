a3aebac532df3e6b2d8d0de0070a73a782dab092
alvv <alvv@demant.com>
2023-03-29 14:57:40 +0200

    Rearange in class and added glob to seach files

--- backend.py
+++ backend.py
@@ -1,10 +1,21 @@
 import ftplib
- 
-HOSTNAME = "10.32.64.74"
-USERNAME = "aoi"
-PASSWORD = "Inspection@2023"
+from glob import glob
+import os
 
+class galaxiamover:
+    def __init__(self):
+        self.hostname = "10.32.64.74"
+        self.username = "aoi"
+        self.password = "Inspection@2023"
+        
+    def open_connection(self):
+        ftp_server = ftplib.FTP(self.hostname, self.username, self.password)
+        
+    def get_files_to_transfer(self, path_to_the_file):
+        glob_input = os.path.join(path_to_the_file,"*.xml")
+        filelist = glob(glob_input)
+        return(filelist)
+        
 
-# Connect FTP Server
-ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
-ftp_server.dir()
+gl = galaxiamover()
+print(gl.get_files_to_transfer('Data'))
